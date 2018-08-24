ecfab0bee7c40e0a981a2441ad26c6e252822f83
E Anatolios <anatolios007@gmail.com>
2018-08-23 23:20:54 -0700

    scanner in and more try catch

--- scannerIn.java
+++ scannerIn.java
@@ -44,3 +44,31 @@ public class Solution {
 
     }
 }
+
+///////////////another try catch
+//https://www.hackerrank.com/challenges/java-exception-handling-try-catch/problem
+public class Solution {
+      
+    static void solve(StringBuilder x, StringBuilder y){
+        try{
+            int a=Integer.parseInt(x.toString());
+            int b=Integer.parseInt(y.toString());
+            int ans= a/b;
+            System.out.println(ans);
+        }catch(NumberFormatException e){
+            System.out.println("java.util.InputMismatchException");
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    } 
+    
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        StringBuilder x=new StringBuilder(sc.nextLine());
+        StringBuilder y=new StringBuilder(sc.nextLine()); 
+        solve(x,y);
+        
+    }
+}
