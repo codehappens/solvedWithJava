718837a9f40f3135318add331e27e40881612784
E Anatolios <anatolios007@gmail.com>
2018-08-25 00:57:05 -0700

    simple switch statement

---
+++ simpleSwitch.java
@@ -0,0 +1,24 @@
+//simple switch statemnt
+import java.lang.Math; 
+
+
+public class HelloWorld
+{
+
+  public static void main(String[] args)
+  {
+    int x=3;
+
+    switch(x){
+      case 1:
+      	System.out.println("Case 1");
+      	break;
+      case 2:
+      	System.out.println("Case 2");
+      	break;
+      default:
+      	System.out.println("Default");
+  }
+}
+}
+
